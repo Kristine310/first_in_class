86685a450dbf51803390f95de0e6e23142b73c93
Kristine Trevino Kinoshita <kristinet@cpp.edu>
2021-09-24 09:30:50 -0700

    Created outline to calculate the restaurant bill

---
+++ restaurantmain.c
@@ -0,0 +1,13 @@
+/*
+For this program, I plan to create a random number generator that will select a number between 0 to 3. Each of the four meals will be stored in a string array and 
+the prices for each will be stored in a double array in the same index its corresponding food is in the string array. The program then gets the data at the randomly
+chosen index as the meal cost that the tax and tip will go by (they may be stored in a separate double variable). 
+
+The program will then prompt the user to input the tax and tip percentages which are then turned into decimals (using percent/100). A double variable will be made 
+to store the tax amount data and it will be calculated by multiplying the decimal form of the tax percentage and the meal cost. This same process will be done for 
+the tip amount. 
+
+Another double variable will be made to add up all of the amounts (meal cost, tax, and tip) for the total cost. 
+
+A few printf statements will then print out the numbers for that data.
+*/
